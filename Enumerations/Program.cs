0d92d34237293a515a9abeca7f2197bf4a9f413b
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 11:17:00 +0700

    Add files via upload

---
+++ Enumerations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+        
+        static void Main(string[] args)
+        {
+            int WeekDateStart = (int)Days.Monday;
+            int WeekDateEnd = (int)Days.Sunday;
+
+            Console.WriteLine("Monday : {0}", WeekDateStart);
+            Console.WriteLine("Sunday : {0}", WeekDateEnd);
+        }
+    }
+}
