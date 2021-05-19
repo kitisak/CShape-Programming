67bd5d23e46fa05ddd18221153550c227ec004a0
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 14:49:40 +0700

    Add files via upload

---
+++ SharedMember/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SharedMember
+{
+    // Static Class
+    public static class TemperatureConverter {
+        // Static Member
+        public static double NumTemperature;
+
+        // Static Method
+        public static double GetTemp
+        {
+            get {
+                return 0.0;
+            }
+        }        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TemperatureConverter.NumTemperature = 15.5);            
+        }
+    }
+}
