1eed201f8ecc7d47fee7915e084ddfb5203e5405
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 08:17:56 +0700

    Add files via upload

---
+++ Checked/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Checked
+{
+    class Program
+    {
+        static int maxIntValue = 2147483647;
+
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            try
+            {
+                result = checked(maxIntValue + 10);
+            }
+            catch (System.OverflowException ex)
+            {
+                Console.WriteLine("Check Integer is Error : " + ex.ToString());
+            }            
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MethodOut/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodOut
+{
+    class Program
+    {
+        //Output Parameter
+        public void getVal(out int x, out int y) {
+            Console.WriteLine("Enter Value 1: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Value 2: ");
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            Program myNum = new Program();
+
+            int num1, num2;
+
+            myNum.getVal(out num1, out num2);
+
+            Console.WriteLine("Value after call method 1: {0}", num1);
+            Console.WriteLine("Value after call method 2: {0}", num2);
+            Console.ReadLine();
+        }
+    }
+}
