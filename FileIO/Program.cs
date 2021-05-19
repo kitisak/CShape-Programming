00015fb2c0d90d028c1fe44c8541320f95f3da5f
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 09:29:29 +0700

    Update Program.cs

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -7,15 +7,37 @@ namespace FileIO
     {
         static void Main(string[] args)
         {
+            /*
+            // Demo 01 : File.Exists
             string path = @"D:\ExamData.txt";
 
             if (File.Exists(path)) {
-                Console.WriteLine("File Readly to Exits !!!");
+                Console.WriteLine("File Readly to Exists !!!");
             } else {
                 Console.WriteLine("File not Found !!!");
             }
 
-            Console.ReadKey();           
+            Console.ReadKey();     
+            */
+
+            // Demo 02 : File.ReadAlllines
+
+            string path = @"D:\ExamData.txt";
+            string[] lines;
+
+            try
+            {                 
+                lines = File.ReadAllLines(path);
+                Console.WriteLine(lines[0]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                Console.ReadKey();
+            }
+            
         }
     }
 }
