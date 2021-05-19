a8f7cf9c11417c5c7dc39d203a5d923173fc463a
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 13:43:00 +0700

    Update Program.cs

--- StructureMethod/Program.cs
+++ StructureMethod/Program.cs
@@ -1,7 +1,27 @@
-﻿using System;
+using System;
 
 namespace StructureMethod
 {
+    struct Teams
+    {
+        public string title;
+        public string driver;
+        public string nationality;
+        public string constructor;
+        public int podium;
+
+        public void getValues(string strTitle, string strDriver, string strNationality,
+            string strConstructor, int intPodium)
+        {
+            title = strTitle;
+            driver = strDriver;
+            nationality = strNationality;
+            constructor = strConstructor;
+            podium = intPodium;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
