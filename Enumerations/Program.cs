1e1ff1ba2192105eb49046940d05c64e388bb304
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 11:26:21 +0700

    Update Program.cs

--- Enumerations/Program.cs
+++ Enumerations/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Enumerations
 {
@@ -8,11 +8,34 @@ namespace Enumerations
         
         static void Main(string[] args)
         {
-            int WeekDateStart = (int)Days.Monday;
-            int WeekDateEnd = (int)Days.Sunday;
+          
+            Console.WriteLine("Sunday : {0}", (int)Days.Sunday);
+            Console.WriteLine("Monda : {0}", (int)Days.Monday);
+            Console.WriteLine("Tuesday : {0}", (int)Days.Tuesday);
+            Console.WriteLine("Wednesday : {0}", (int)Days.Wednesday);
+            Console.WriteLine("Thursday : {0}", (int)Days.Thursday);
+            Console.WriteLine("Friday : {0}", (int)Days.Friday);
+            Console.WriteLine("Saturday : {0}", (int)Days.Saturday);
 
-            Console.WriteLine("Monday : {0}", WeekDateStart);
-            Console.WriteLine("Sunday : {0}", WeekDateEnd);
+            Console.WriteLine("---------------------------------------------");
+
+            Console.WriteLine("Sunday : {0}", Days.Sunday);
+            Console.WriteLine("Monda : {0}", Days.Monday);
+            Console.WriteLine("Tuesday : {0}", Days.Tuesday);
+            Console.WriteLine("Wednesday : {0}", Days.Wednesday);
+            Console.WriteLine("Thursday : {0}", Days.Thursday);
+            Console.WriteLine("Friday : {0}", Days.Friday);
+            Console.WriteLine("Saturday : {0}", Days.Saturday);
+            Console.ReadKey();
+
+            /*
+          int WeekDateStart = (int)Days.Monday;
+          int WeekDateEnd = (int)Days.Sunday;
+
+          Console.WriteLine("Monday : {0}", WeekDateStart);
+          Console.WriteLine("Sunday : {0}", WeekDateEnd);
+          */
+
         }
     }
 }
