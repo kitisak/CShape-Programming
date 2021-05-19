fc3896c1520c0000db67103dfd145c42b877fd18
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-19 08:45:44 +0700

    Update Program.cs

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -7,29 +7,7 @@ namespace FileIO
     {
         static void Main(string[] args)
         {
-            FileStream myFile = new FileStream("testdata.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-
-            try
-            {
-                for (int i = 1; i <= 10; i++)
-                {
-                    myFile.WriteByte((byte)i);
-                }
-
-                myFile.Position = 0;
-                for (int i = 0; i <= 10; i++)
-                {
-                    Console.Write(myFile.ReadByte() + " ");
-                }
-            }
-            catch (IOException ex)
-            {
-                Console.Write(ex.Message);
-            }
-            finally {
-                myFile.Close();
-                Console.ReadKey();
-            }           
+            
         }
     }
 }
