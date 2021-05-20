698f12c11a8b70b4c8a6dc457cf0a50f4cef86a2
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 14:08:20 +0700

    Update Program.cs

--- OverloadOperation/Program.cs
+++ OverloadOperation/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using System;
 
 namespace OverloadOperation
 {
+    public class ComplexNumber
+    {
+        private int myInt1;
+        private int myInt2;
+
+        public ComplexNumber() { }
+
+        public ComplexNumber (int myNum1, int myNum2)
+        {
+            myInt1 = myNum1;
+            myInt2 = myNum2;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("{0} {1}", myInt1, myInt2);
+            Console.ReadKey();
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ComplexNumber myComplex1 = new ComplexNumber(50, 100);
+            myComplex1.Display();
         }
     }
 }
