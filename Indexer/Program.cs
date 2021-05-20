9130e47739fe1f4ffd1b084fb44a24253d3d0e15
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 13:19:59 +0700

    Update Program.cs

--- Indexer/Program.cs
+++ Indexer/Program.cs
@@ -1,11 +1,22 @@
-﻿using System;
+using System;
 
 namespace Indexer
 {
     public class TempRecord
     {
+        // Field - Array (Collection)
+        float[] temps = new float[5] { 10.5F, 20.5F, 30.5F, 40.5F, 50.5F };
 
+        // => คือ Lambda Expression (inputparameter) => (Expression)
+        public int Length => temps.Length;
 
+        // Indexer ทำหน้า get/set ค่าใช้ตัวแปรในคลาส ที่เป็น Collection Class จะเป็นการใช้ index 
+        // Indexer จะชื่อ this
+        public float this[int index]
+        {
+            get => temps[index];
+            set => temps[index] = value;
+        }
     }
 
     class Program
