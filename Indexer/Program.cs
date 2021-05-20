d3cf30d6253b33676ac4373311c0072b3f69e55c
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 13:10:32 +0700

    Add files via upload

---
+++ Indexer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Indexer
+{
+    public class TempRecord
+    {
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
