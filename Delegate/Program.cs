4d61da4372a1eded70efc86b9661a15441f3be0e
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 15:21:36 +0700

    Update Program.cs

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+using System;
 
 namespace Delegate
 {
+    public class Teams
+    {
+        public static void DispalyTeams(string message)
+        {
+            Console.WriteLine("Call Teams.DisplayTeams() with :" + message);
+        }
+    }
+
+    public class Grandprix
+    {
+        public static void DispalyGP(string message)
+        {
+            Console.WriteLine("Call Grandprix.DispalyGP() with :" + message);
+        }
+    }
+
+    // Define Delegate
+    public delegate void MyDelegate(string msg);
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyDelegate del;
+                
+            del = Teams.DispalyTeams;
+            del("Redbull Racing");
+
+            del = Grandprix.DispalyGP;
+            del("TuscanGP");
+
+            del = (string msg) => Console.WriteLine("Call Lambda : " + msg);
+            del("Alex Albon");
         }
     }
 }
