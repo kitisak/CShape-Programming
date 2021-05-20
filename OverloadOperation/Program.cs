0faaa0fc5211844ffb3b90818fe8540295a3d503
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 14:05:46 +0700

    Add files via upload

---
+++ OverloadOperation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OverloadOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
