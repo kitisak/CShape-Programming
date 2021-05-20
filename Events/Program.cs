f29ee375b003df92ffc9f331efe343260d1c2078
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-20 15:39:13 +0700

    Update Program.cs

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+using System;
+
+public delegate void OnBoardF1TeamHandler(object sender, EventArgs e);
 
 namespace Events
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] teams = { "Mercides", "Redbull", "Alpine", "Ferrari" };
+
+            F1Event f1 = new F1Event();
+            f1.OnBoardF1TeamEvent += new OnBoardF1TeamHandler(callback);
+
+            Random random = new Random();
+
+            Console.WriteLine("Resualt forcast team win");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int rd = random.Next(teams.Length);
+
+                Console.WriteLine("Grandprix " i);
+
+                if (teams[rd] == "Redbull" || teams[rd] == "Mercides")
+                {
+                    f1.OnBoardF1TeamEvent();
+                }
+            }
+        }
+
+        public static void callback(object sender, EventArgs e)
+        {
+            Console.WriteLine("This F1 teams onboard GrandPrix.");
+        }
+    }
+
+    public class F1Event
+    {
+        public event OnBoardF1TeamHandler TeamF1Event;
+
+        public void OnBoardF1TeamEvent() {
+            if (TeamF1Event != null)
+                TeamF1Event(this, EventArgs.Empty);
         }
     }
 }
