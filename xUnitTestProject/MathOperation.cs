7cc39348bc26ea5c6619d1e0e8feb7c66381bbbc
Tizcom <45397970+Tizcom@users.noreply.github.com>
2021-05-21 14:56:45 +0700

    Add files via upload

---
+++ xUnitTestProject/MathOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTestProject
+{
+    public static class MathOperation
+    {
+        public static double Add(double myNum1, double myNum2)
+        {
+            return (myNum1 + myNum2);
+        }
+
+        public static double Subtract(double myNum1, double myNum2)
+        {
+            return (myNum1 - myNum2);
+        }
+
+        public static double Multiply(double myNum1, double myNum2)
+        {
+            return (myNum1 * myNum2);
+        }
+
+        public static double Divide(double myNum1, double myNum2)
+        {
+            return (myNum1 / myNum2);
+        }
+    }
+}
